361ac14200b7dc0069381bc5f3fdd6fb406ac1a0
Mariano Vazquez <mareanovazquez@protonmail.com>
2026-03-16 15:13:49 -0300

    convert to PWA

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,6 @@
+import { registerSW } from 'virtual:pwa-register'
+registerSW({ immediate: true })
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
